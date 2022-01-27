be19d60f84f84bf084efca8c74e88eb0338157bb
Shamiya Lin <shamiya2023@gmail.com>
2022-01-27 08:35:41 -0600

    added constructor and win method

--- tic tac toe/src/com/company/Game.java
+++ tic tac toe/src/com/company/Game.java
@@ -2,9 +2,26 @@ package com.company;
 
 public class Game {
     String[][][] board = new String[4][4][4]; //[s (sheet)],[r (row)], [c (collumn)
+    boolean win = false;
+    Game(){
+        // E = empty, F = filled
+
+        for(int sheet = 0; sheet < 4; sheet++){
+            for(int row = 0; row < 4; row++){
+                for(int col = 0; col < 4; col++){
+                    board[sheet][row][col] = "E";
+                }
+            }
+        }
+
+    }
 
     public String[][][] getBoard() {
         return board;
+    }
+
+    public boolean win(){
+        return win;
     }
 
     //include win methods
