1d2dafe9330990b6cfe96dcddf048d333bfa82eb
ta-v <tavishee.s@gmail.com>
2022-02-23 11:14:43 -0600

    generate rnadom location in BlanketFort

--- tic tac toe/src/com/company/BlanketFort.java
+++ tic tac toe/src/com/company/BlanketFort.java
@@ -1,5 +1,22 @@
 package com.company;
 
-public class BlanketFort extends Game3{
+public class BlanketFort extends Game3
+{
+    public Location generateRandomLocation()
+    {
 
+        int rSheet = (int) (Math.random()*(3-0+1)+0);
+        int rRow = (int) (Math.random()*(3-0+1)+0);
+        int rCol = (int) (Math.random()*(3-0+1)+0);
+
+        while (board[rSheet][rRow][rCol] != '-')
+        {
+            rSheet = (int) (Math.random()*(3-0+1)+0);
+            rRow = (int) (Math.random()*(3-0+1)+0);
+            rCol = (int) (Math.random()*(3-0+1)+0);
+
+        }
+        Location adding = new Location(rSheet, rRow, rCol);
+        return adding;
+    }
 }
